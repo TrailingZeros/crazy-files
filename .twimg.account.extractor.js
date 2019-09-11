dd7f5c424d8754d40e9a0e8413b0c6dac3924b4e
TrailingZeros <TrailingZeros@localhost>
2019-09-11 05:24:00 +0000

    .

---
+++ .twimg.account.extractor.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+const puppeteer = require("puppeteer");
+const axios = require("axios");
+const fs = require("fs");
+
+const args = process.argv.slice(2);
+const account = args[0];
+
+function wait(ms) {
+  return new Promise((resolve, reject) => setTimeout(resolve, ms));
+}
+
+function scanMatches(re, s) {
+  return s.match(re);
+}
+
+// https://pbs.twimg.com/media/EEGVKodUcAAsVpl.png:small
+const imgRegex = /(?:https?:)?\/\/pbs\.twimg\.com\/media\/([^.]+\.(?:png|jpe?g))(?:\:[^\" <>]+)?/;
+const tweetRegex = /(?:https?:\/\/(?:www\.|m\.|mobile\.)?twitter\.com)?\/(?:[a-zA-Z0-9_]+)\/status\/(?:[0-9]+)/g;
+
+(async () => {
+  console.log("Opening");
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--disable-dev-shm-usage"].concat(
+      process.env.CI ? ["--no-sandbox", "--disable-setuid-sandbox"] : []
+    )
+  });
+  try {
+    const page = await browser.newPage();
+    await page.setJavaScriptEnabled(true);
+    await page.goto(
+      // https://twitter.com/search?q=from%3Ajapan%20filter%3Avideos&src=typd
+      `https://twitter.com/${account}/media`,
+      {
+        timeout: 0
+      }
+    );
+    await page.setRequestInterception(true);
+    page.on("request", interceptedRequest => {
+      const url = interceptedRequest.url();
+      if (imgRegex.test(url)) {
+        const imgName = url.match(imgRegex)[1];
+        console.log(`Found: ${imgName}`);
+        const filename = `twitter-${account}-${imgName}`;
+        axios({ url, responseType: "arraybuffer" })
+          .then(({ data }) => fs.writeFileSync(filename, data))
+          .catch(console.log);
+      }
+      interceptedRequest.continue();
+    });
+    let loadFlag = true;
+    let count = 0;
+    while (loadFlag) {
+      count++;
+      console.error(`Processing #${count}`);
+      await page.evaluate(_ => {
+        window.scrollTo(0, document.body.scrollHeight);
+        document.querySelectorAll("video,iframe").forEach(function(item) {
+          item.remove();
+        });
+      });
+      const waitTime = parseInt(5000 + Math.random() * 2000);
+      console.error(`Waiting ${waitTime}ms`);
+      await wait(waitTime);
+      loadFlag = count < 5;
+    }
+  } finally {
+    await browser.close();
+  }
+})().catch(console.err);
