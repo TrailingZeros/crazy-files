1985cc6fb0c4392ec6500e1aab8eb7b05666485b
TrailingZeros <TrailingZeros@localhost>
2019-09-13 01:16:12 +0000

    .

--- .twimg.account.extractor.js
+++ .twimg.account.extractor.js
@@ -1,18 +1,21 @@
 #!/usr/bin/env node
+const socks = require("socks-proxy-agent");
 const puppeteer = require("puppeteer");
-const axios = require("axios");
+const axios_ = require("axios");
 const fs = require("fs");
+const net = require("net");
 
 const args = process.argv.slice(2);
 const account = args[0];
 
+const proxy = "socks5://localhost:9050";
+const agent = socks(proxy);
+const axios = axios_.create({ httpsAgent: agent, httpAgent: agent });
+
 function wait(ms) {
-  return new Promise((resolve, reject) => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function scanMatches(re, s) {
-  return s.match(re);
-}
 function retry(func, cnt = 0) {
   return func().catch(r => {
     if (cnt > 3) {
@@ -22,28 +25,51 @@ function retry(func, cnt = 0) {
   });
 }
 
+function reloadCircuit() {
+  return new Promise(resolve => {
+    const client = new net.Socket();
+    client.connect(9051, "127.0.0.1", function() {
+      client.write('AUTHENTICATE ""\nSIGNAL NEWNYM');
+    });
+
+    client.on("data", function() {
+      client.destroy(); // kill client after server's response
+    });
+
+    client.on("close", function() {
+      resolve();
+    });
+  });
+}
+
+async function randomWait() {
+  const waitTime = parseInt(5000 + Math.random() * 2000);
+  console.error(`Waiting ${waitTime}ms`);
+  await wait(waitTime);
+}
+function showMyIp() {
+  return axios({ url: "http://checkip.amazonaws.com/" })
+    .then(a => a.data.trim())
+    .then(console.log);
+}
+
 // https://pbs.twimg.com/media/EEGVKodUcAAsVpl.png:small
 const imgRegex = /(?:https?:)?\/\/pbs\.twimg\.com\/media\/([^.]+\.(?:png|jpe?g))(?:\:[^\" <>]+)?/;
-const tweetRegex = /(?:https?:\/\/(?:www\.|m\.|mobile\.)?twitter\.com)?\/(?:[a-zA-Z0-9_]+)\/status\/(?:[0-9]+)/g;
 
 (async () => {
+  await showMyIp();
   console.log("Opening");
   const browser = await puppeteer.launch({
     headless: true,
-    args: ["--disable-dev-shm-usage"].concat(
+    args: ["--disable-dev-shm-usage", `--proxy-server=${proxy}`].concat(
       process.env.CI ? ["--no-sandbox", "--disable-setuid-sandbox"] : []
     )
   });
+  const url = `https://twitter.com/${account}/media`;
   try {
     const page = await browser.newPage();
     await page.setJavaScriptEnabled(true);
-    await page.goto(
-      // https://twitter.com/search?q=from%3Ajapan%20filter%3Avideos&src=typd
-      `https://twitter.com/${account}/media`,
-      {
-        timeout: 0
-      }
-    );
+    await page.goto(url, { timeout: 0 });
     await page.setRequestInterception(true);
     let loadFlag = true;
     page.on("request", interceptedRequest => {
@@ -56,7 +82,7 @@ const tweetRegex = /(?:https?:\/\/(?:www\.|m\.|mobile\.)?twitter\.com)?\/(?:[a-z
           axios({ url, responseType: "arraybuffer" }).then(({ data }) =>
             fs.writeFileSync(filename, data)
           )
-        ).catch(a => console.log(a.response));
+        ).catch(console.log);
         loadFlag = true;
       }
       interceptedRequest.continue();
@@ -72,9 +98,15 @@ const tweetRegex = /(?:https?:\/\/(?:www\.|m\.|mobile\.)?twitter\.com)?\/(?:[a-z
           item.remove();
         });
       });
-      const waitTime = parseInt(5000 + Math.random() * 2000);
-      console.error(`Waiting ${waitTime}ms`);
-      await wait(waitTime);
+      await randomWait();
+      if (count == 1 && !loadFlag && proxy) {
+        console.log("Trying to reload Tor circuits");
+        while (loadFlag) {
+          await reloadCircuit();
+          await Promise.all(showMyIp(), page.goto(url, { timeout: 0 }));
+          await randomWait();
+        }
+      }
     }
   } finally {
     await browser.close();
