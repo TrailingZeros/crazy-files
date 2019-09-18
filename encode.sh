cf4db935d95b9b0d069b67b90b81f55b3a0793b5
TrailingZeros <TrailingZeros@localhost>
2019-09-18 07:50:43 +0000

    .

--- encode.sh
+++ encode.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 IN="$1"
 OUT="${IN%.*}.webm"
-/usr/bin/ffmpeg -i "${IN}" -vcodec libvpx -acodec libopus -ac 2 -r 30 -max_muxing_queue_size 1024 "${OUT}"
+/usr/bin/ffmpeg -i "${IN}" -vcodec libvpx -acodec libopus -ac 2 -r 30 -max_muxing_queue_size 1024 \
+    -map_chapters -1 -map_metadata -1 "${OUT}"
