47859ab6d01a13ba0dbf5d616da707a23248b6da
TrailingZeros <TrailingZeros@localhost>
2019-10-19 02:31:06 +0000

    .

--- presplit.sh
+++ presplit.sh
@@ -2,6 +2,6 @@
 set -xe
 find . -maxdepth 3 -size +1000000000c -print | grep -vE '\.split$' | \
     grep -vE '.+\.f[0-9]+\..+\.part' | while read i ; do
-  split -a 3 -b 100M -d --additional-suffix=.split --verbose "$i" "${i}."
+  split -a 3 -b 10M -d --additional-suffix=.split --verbose "$i" "${i}."
   rm "$i"
 done
