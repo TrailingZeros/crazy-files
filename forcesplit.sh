be2bf6764af82e9a5818a6a9ca25a4072cba3aac
TrailingZeros <TrailingZeros@localhost>
2019-10-29 11:49:13 +0000

    .

--- forcesplit.sh
+++ forcesplit.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -xe
-find . -maxdepth 3 -size +500000000c -print | grep -vE '\.split$' | \
-    grep -v 'audio/' | grep -v 'ok/' | \
+for j in "$@" ; do echo "$j" ; done | grep -vE '\.split$' | \
     grep -vE '.+\.f[0-9]+\..+\.part' | while read i ; do
   split -a 3 -b 10M -d --additional-suffix=.split --verbose "$i" "${i}."
   rm "$i"
