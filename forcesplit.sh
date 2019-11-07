bba545602849fc4c22e487b35e87489703b491b6
TrailingZeros <TrailingZeros@localhost>
2019-11-07 08:04:33 +0000

    .

--- forcesplit.sh
+++ forcesplit.sh
@@ -2,6 +2,6 @@
 set -xe
 for j in "$@" ; do echo "$j" ; done | grep -vE '\.split$' | \
     grep -vE '.+\.f[0-9]+\..+\.part' | while read i ; do
-  split -a 3 -b 10M -d --additional-suffix=.split --verbose "$i" "${i}."
+  split -a 5 -b 10M -d --additional-suffix=.split --verbose "$i" "${i}."
   rm "$i"
 done
