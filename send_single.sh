adf5ad27edf358dc2c9b70da2419222a5b01e62d
TrailingZeros <TrailingZeros@localhost>
2019-11-12 03:30:54 +0000

    .

--- send_single.sh
+++ send_single.sh
@@ -5,5 +5,10 @@ git -C "${2:-bkup}" init || true
 git -C "${2:-bkup}" add .
 git -C "${2:-bkup}" commit -am. || true
 git -C "${2:-bkup}" remote add origin "$REPO/${VIDEOS_PREFIX:-data}-${1:-1}.git" || true
-git -C "${2:-bkup}" push -u origin master
+n=0
+until [ $n -ge 5 ]
+do
+  git -C "${2:-bkup}" push -u origin master && break
+  n=$[$n+1]
+done
 rm -rf "${2:-bkup}"
