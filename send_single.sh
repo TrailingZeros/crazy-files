7de3c4710d9d39ac7f99cac27d08b4a36c535730
TrailingZeros <TrailingZeros@localhost>
2019-11-13 04:17:10 +0000

    .

--- send_single.sh
+++ send_single.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 set -ex
 echo "$REPO/data-${1:-1}.git"
-git -C "${2:-bkup}" init || true
+if git -C "${2:-bkup}" init | grep Reinit &>/dev/null ; then
+  true
+else
+  git -C "${2:-bkup}" remote add origin "$REPO/${VIDEOS_PREFIX:-data}-${1:-1}.git" || true
+  for II in $REPO{1..10} ; do
+    git -C "${2:-bkup}" config --add remote.origin.url "$II/${VIDEOS_PREFIX:-data}-$1.git"
+  done
+fi
 git -C "${2:-bkup}" add .
 git -C "${2:-bkup}" commit -am. || true
-git -C "${2:-bkup}" remote add origin "$REPO/${VIDEOS_PREFIX:-data}-${1:-1}.git" || true
+
 n=0
 until [ $n -ge 5 ]
 do
